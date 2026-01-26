d2d140837edefc3c4bb5b4a2792ce0fc05f04e35
Arezou.hadi@ucalgary.ca <arezou.hadi@ucalgary.ca>
2026-01-26 09:52:47 -0700

    .py files

--- src/goph547lab00/arrays.py
+++ src/goph547lab00/arrays.py
@@ -7,14 +7,6 @@ def square_ones(n):
 
 def load_images(path):
     """Loads a color image and its grayscale version."""
-    # Load color image
     img = np.asarray(Image.open(path))
-    # Load grayscale version
     gray_img = np.asarray(Image.open(path).convert("L"))
     return img, gray_img
-
-def crop_pinnacle(gray_img):
-    """Crops the grayscale image to isolate the rock pillar."""
-    # Adjust these indices based on your specific image dimensions
-    small_gray_image = gray_img[300:700, 100:400]
-    return small_gray_image
